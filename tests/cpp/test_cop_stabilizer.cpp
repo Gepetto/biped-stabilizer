690b7419c47da0fe9051d8f87c43d3f9dea4ebf9
Maximilien Naveau <maximilien.naveau@gmail.com>
2025-09-24 14:36:26 +0200

    Migrate C++ test to doctest

--- tests/cpp/test_cop_stabilizer.cpp
+++ tests/cpp/test_cop_stabilizer.cpp
@@ -1,13 +1,10 @@
-#define BOOST_TEST_MODULE biped_stabilizer test boost
-#include <boost/test/included/unit_test.hpp>
+#define DOCTEST_CONFIG_IMPLEMENT_WITH_MAIN
+#include "doctest/doctest.h"
 
 #include "biped-stabilizer/cop_stabilizer.hpp"
 
-BOOST_AUTO_TEST_CASE(test_default_constructor) {
+TEST_CASE("FlexEstimatorTest - DefaultConstructor") {
   biped_stabilizer::CopStabilizer stabilizer;
   biped_stabilizer::CopStabilizerSettings default_settings;
-  BOOST_CHECK(true);
-  BOOST_CHECK(default_settings == stabilizer.getSettings());
+  CHECK(default_settings == stabilizer.getSettings());
 }
-
-BOOST_AUTO_TEST_CASE(test_dummy_2) { BOOST_CHECK(true); }
